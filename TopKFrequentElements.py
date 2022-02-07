5b225a0ad3035cdd9a6ecb383f6757f4a43b65f6
melo <melolinchou@gmail.com>
2022-02-07 11:56:42 +0800

    Top K Frequent Elements

---
+++ TopKFrequentElements.py
@@ -0,0 +1,14 @@
+from ast import List
+from collections import Counter
+
+
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    counter = Counter(nums)
+    maxK = []
+    for i in range(0, k):
+        maxKey = max(counter, key=counter.get)
+        maxK.append(maxKey)
+        counter.pop(maxKey)
+    return maxK
+
+topKFrequent([1,1,1,2,2,3],2)
